2fef80fd8049d22e59e77fb1124c8a0607fd76a7
GmtDT <dylanturnerfly@gmail.com>
2024-09-24 09:52:42 -0400

    Validating Rotation functions implemented

--- Assignment_1.py
+++ Assignment_1.py
@@ -2,6 +2,10 @@
 # Assignment 1
 # Dylan Turner & Noor Hasan
 
+import numpy as np
+import numpy.linalg 
+import matplotlib.pyplot as plt
+
 ############################################################
 #                  Validating Rotations                    #
 ############################################################
@@ -11,15 +15,66 @@
 
 #Input: matrix ∈ ×. Return: True if m∈SO(n) (within epsilon numerical preci-sion), false otherwise. (Consider n=2 or n=3 only)
 def check_SOn(matrix, epsilon = 0.01):
-    return True
+    #Must be a square matrix.
+    if(matrix.shape[0] != matrix.shape[1]):
+        return False
+    
+    #Must be orthagonal: R^T * R = I
+    identity = np.eye(matrix.shape[0])  
+    orthogonal_check = np.allclose(np.dot(matrix.T, matrix), identity, atol=epsilon)
 
-# Input: vector ∈ ×. Return: True if  ∈S3 (within epsilon numericalprecision), false otherwise
+    #Determinant must equal 1
+    determinant_check = np.isclose(np.linalg.det(matrix), 1, atol=epsilon)
+
+    return determinant_check and orthogonal_check
+
+#Examples
+# R = np.array([[1, 0], [0, 1]])  
+# print(check_SOn(R, epsilon=1e-6))
+# R2 = np.array([[1, 0], [0, 0.5]])
+# print(check_SOn(R2, epsilon=1e-6))
+
+# Input: vector ∈ ×. Return: True if vector ∈ S3 (within epsilon numericalprecision), false otherwise
 def check_quaternion(vector, epsilon = 0.01):
-    return True
+    #Quaternion vector must be length 4
+    if len(vector) != 4:
+        return False
+    
+    magnitude = np.sqrt(np.sum(np.square(vector)))
+    return np.isclose(magnitude, 1, atol=epsilon)
+
+# q = [1, 0, 0, 0]  
+# print(check_quaternion(q))
+# q2 = [1, 1, 0, 1]  
+# print(check_quaternion(q2))
+
 
 # Input: matrix ∈×. Return: True if ∈SE(n) (within epsilon numerical precision),false otherwise. (Consider n=2 or n=3 only)
 def check_SEn(matrix, epsilon = 0.01):
+    #Make sure the rotation part of the matrix is valid
+    n = matrix.shape[0] - 1 #dim of rot matrix
+
+    if matrix.shape != (n + 1, n + 1): #correct shape
+        return False
+    
+    rotation_matrix = matrix[:n, :n] #extract
+
+    if not check_SOn(rotation_matrix, epsilon):
+        return False
+    
+    #Make sure the bottom row is [0, 0, ..., 0, 1]
+    bottom_row = matrix[n, :]
+    expected_bottom_row = np.zeros(n + 1)
+    expected_bottom_row[-1] = 1 
+    
+    if not np.allclose(bottom_row, expected_bottom_row, atol=epsilon):
+        return False
+
+    #All good at this point    
     return True
+
+T = np.array([[1, 0, 0, 3], [0, 1, 0, 4], [0, 0, 1, 5], [0, 0, 0, 1]])  # SE(3) matrix
+print(check_SEn(T))  # Output: True
 
 
 #EXTRA CREDIT
